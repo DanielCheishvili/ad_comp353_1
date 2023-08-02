ba59591bc2ad1321f834e89cc920c6cbefcfbb92
DanielCheishvili <60618343+DanielCheishvili@users.noreply.github.com>
2023-08-02 14:46:35 -0400

    test

--- index.php
+++ index.php
@@ -11,40 +11,40 @@
     <h1> Create A Person </h1>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
         <label for="medicareCard">Medicare Card</label>
-        <input type="text" name="medicareCard" id="medicareCard" placeholder="Medicare Card">
+        <input type="text" name="medicareCard" id="medicareCard" placeholder="Medicare Card" value="<?php echo htmlspecialchars($medicareCard); ?>">
         <br>
         <label for="firstName">First Name</label>
-        <input type="text" name="firstName" id="firstName" placeholder="First Name">
+        <input type="text" name="firstName" id="firstName" placeholder="First Name" value="<?php echo htmlspecialchars($firstName); ?>">
         <br>
         <label for="lastName">Last Name</label>
-        <input type="text" name="lastName" id="lastName" placeholder="Last Name">
+        <input type="text" name="lastName" id="lastName" placeholder="Last Name" value="<?php echo htmlspecialchars($lastName); ?>">
         <br>
         <label for="address">Address</label>
-        <input type="text" name="address" id="address" placeholder="Address">
+        <input type="text" name="address" id="address" placeholder="Address" value="<?php echo htmlspecialchars($address); ?>">
         <br>
         <label for="city">City</label>
-        <input type="text" name="city" id="city" placeholder="City">
+        <input type="text" name="city" id="city" placeholder="City" value="<?php echo htmlspecialchars($city); ?>">
         <br>
         <label for="province">Province</label>
-        <input type="text" name="province" id="province" placeholder="Province">
+        <input type="text" name="province" id="province" placeholder="Province" value="<?php echo htmlspecialchars($province); ?>">
         <br>
         <label for="postalCode">Postal Code</label>
-        <input type="text" name="postalCode" id="postalCode" placeholder="Postal Code">
+        <input type="text" name="postalCode" id="postalCode" placeholder="Postal Code" value="<?php echo htmlspecialchars($postalCode); ?>">
         <br>
         <label for="telephoneNumber">Telephone Number</label>
-        <input type="text" name="telephoneNumber" id="telephoneNumber" placeholder="Telephone Number">
+        <input type="text" name="telephoneNumber" id="telephoneNumber" placeholder="Telephone Number" value="<?php echo htmlspecialchars($telephoneNumber); ?>">
         <br>
         <label for="email">Email</label>
-        <input type="text" name="email" id="email" placeholder="Email">
+        <input type="text" name="email" id="email" placeholder="Email" value="<?php echo htmlspecialchars($email); ?>">
         <br>
         <label for="dateOfBirth">Date Of Birth</label>
-        <input type="text" name="dateOfBirth" id="dateOfBirth" placeholder="Date Of Birth">
+        <input type="text" name="dateOfBirth" id="dateOfBirth" placeholder="Date Of Birth" value="<?php echo htmlspecialchars($dateOfBirth); ?>">
         <br>
         <label for="medicareExpiryDate">Medicare Expiry Date</label>
-        <input type="text" name="medicareExpiryDate" id="medicareExpiryDate" placeholder="Medicare Expiry Date">
+        <input type="text" name="medicareExpiryDate" id="medicareExpiryDate" placeholder="Medicare Expiry Date" value="<?php echo htmlspecialchars($medicareExpiryDate); ?>">
         <br>
         <label for="citizenship">Citizenship</label>
-        <input type="checkbox" name="citizenship" id="citizenship" placeholder="Citizenship">
+        <input type="checkbox" name="citizenship" id="citizenship" placeholder="Citizenship" value="<?php echo htmlspecialchars($citizenship); ?>">
         <br>
         <input type="submit" name="submit" value="Submit">
     </form>
@@ -56,11 +56,9 @@
 
         $medicareCard_err = $firstName_err = $lastName_err = $address_err = $city_err = $province_err = $postalCode_err = 
         $telephoneNumber_err = $email_err =  $dateOfBirth_err = $medicareExpiryDate_err = $citizenship_err = "";
-        echo "Hello";
-        echo $_POST['medicareCard'];
+        
         if(isset($_POST['submit']))
         {
-            echo "not empty";
             $medicareCard = $_POST['medicareCard'];
             $firstName = $_POST['firstName'];
             $lastName = $_POST['lastName'];
