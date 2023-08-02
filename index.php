1a5b3cd2a8bb2923b1694543ae750b7ae1e29fa0
DanielCheishvili <60618343+DanielCheishvili@users.noreply.github.com>
2023-08-02 12:18:18 -0400

    Form

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
 <?php
 include ('config.php');
 
-//display all students on the page
-$sql = "SELECT * FROM Student";
-$result = mysqli_query($conn, $sql);
+// //display all students on the page
+// $sql = "SELECT * FROM Student";
+// $result = mysqli_query($conn, $sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo "id: " . $row["studentID"]. " - medicare Card: " . $row["medicareCard"]. "<br>";
-    }
-  } else {
-    echo "0 results";
-  }
-  $conn->close();
+// if ($result->num_rows > 0) {
+//     // output data of each row
+//     while($row = $result->fetch_assoc()) {
+//       echo "id: " . $row["studentID"]. " - medicare Card: " . $row["medicareCard"]. " " . $row["lastname"]. "<br>";
+//     }
+//   } else {
+//     echo "0 results";
+//   }
+//   $conn->close();
+
+//Form to create a new student
+echo "<form action='create.php' method='post'>";
+echo "First name: <input type='text' name='firstname'><br>";
+echo "Last name: <input type='text' name='lastname'><br>";
+echo "Medicare Card: <input type='text' name='medicareCard'><br>";
+echo "<input type='submit'>";
+echo "</form>";
+
 
 
 
