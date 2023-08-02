1da47ae36da1ff78aa7450c23658d12e91a4ec17
DanielCheishvili <60618343+DanielCheishvili@users.noreply.github.com>
2023-08-02 13:34:35 -0400

    Form for creating person

---
+++ createPerson.php
@@ -0,0 +1,8 @@
+<?php 
+        include ('config.php');
+        $medicareCard = $firstName = $lastName = $address = $city = $province = 
+        $postalCode = $telephoneNumber = $email =  $dateOfBirth = $medicareExpiryDate = $citizenship = "";
+
+        $medicareCard_err = $firstName_err = $lastName_err = $address_err = $city_err = $province_err = $postalCode_err = 
+        $telephoneNumber_err = $email_err =  $dateOfBirth_err = $medicareExpiryDate_err = $citizenship_err = "";
+?>
--- index.php
+++ index.php
@@ -1,45 +1,56 @@
-<?php
-include ('config.php');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-// //display all students on the page
-// $sql = "SELECT * FROM Student";
-// $result = mysqli_query($conn, $sql);
+    <h1> Create A Person </h1>
+    <form action="createPerson.php" method="POST">
+        <label for="medicareCard">Medicare Card</label>
+        <input type="text" name="medicareCard" id="medicareCard" placeholder="Medicare Card">
+        <br>
+        <label for="firstName">First Name</label>
+        <input type="text" name="firstName" id="firstName" placeholder="First Name">
+        <br>
+        <label for="lastName">Last Name</label>
+        <input type="text" name="lastName" id="lastName" placeholder="Last Name">
+        <br>
+        <label for="address">Address</label>
+        <input type="text" name="address" id="address" placeholder="Address">
+        <br>
+        <label for="city">City</label>
+        <input type="text" name="city" id="city" placeholder="City">
+        <br>
+        <label for="province">Province</label>
+        <input type="text" name="province" id="province" placeholder="Province">
+        <br>
+        <label for="postalCode">Postal Code</label>
+        <input type="text" name="postalCode" id="postalCode" placeholder="Postal Code">
+        <br>
+        <label for="telephoneNumber">Telephone Number</label>
+        <input type="text" name="telephoneNumber" id="telephoneNumber" placeholder="Telephone Number">
+        <br>
+        <label for="email">Email</label>
+        <input type="text" name="email" id="email" placeholder="Email">
+        <br>
+        <label for="dateOfBirth">Date Of Birth</label>
+        <input type="text" name="dateOfBirth" id="dateOfBirth" placeholder="Date Of Birth">
+        <br>
+        <label for="medicareExpiryDate">Medicare Expiry Date</label>
+        <input type="text" name="medicareExpiryDate" id="medicareExpiryDate" placeholder="Medicare Expiry Date">
+        <br>
+        <label for="citizenship">Citizenship</label>
+        <input type="checkbox" name="citizenship" id="citizenship" placeholder="Citizenship">
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+    <br>
 
-// if ($result->num_rows > 0) {
-//     // output data of each row
-//     while($row = $result->fetch_assoc()) {
-//       echo "id: " . $row["studentID"]. " - medicare Card: " . $row["medicareCard"]. " " . $row["lastname"]. "<br>";
-//     }
-//   } else {
-//     echo "0 results";
-//   }
-//   $conn->close();
-
-//Form to create a new student
-echo "<form action='create.php' method='post'>";
-echo "First name: <input type='text' name='firstname'><br>";
-echo "Last name: <input type='text' name='lastname'><br>";
-echo "Medicare Card: <input type='text' name='medicareCard'><br>";
-echo "<input type='submit'>";
-echo "</form>";
 
 
-
-
-?>
-
-
-
-
-<!-- <HTML>
-<HEAD>
- <TITLE>Date/Time Functions Demo</TITLE>
-</HEAD>
-<BODY>
-<H1>Date/Time Functions Demo</H1>
-<P>The current date and time is
-<EM><?echo date("D M d, Y H:i:s", time())?></EM>
-<P>Current PHP version:
-<EM><?echo  phpversion()?></EM>
-</BODY>
-</HTML> -->
+</body>
+</html>
