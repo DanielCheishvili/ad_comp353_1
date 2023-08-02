c39e556283c28ed0479931c55ab0dd1f48447643
DanielCheishvili <60618343+DanielCheishvili@users.noreply.github.com>
2023-08-02 13:55:34 -0400

    creating person

--- createPerson.php
+++ createPerson.php
@@ -5,4 +5,29 @@
 
         $medicareCard_err = $firstName_err = $lastName_err = $address_err = $city_err = $province_err = $postalCode_err = 
         $telephoneNumber_err = $email_err =  $dateOfBirth_err = $medicareExpiryDate_err = $citizenship_err = "";
+
+        if(isset($_POST['POST']))
+        {
+            $medicareCard = $_POST['medicareCard'];
+            $firstName = $_POST['firstName'];
+            $lastName = $_POST['lastName'];
+            $address = $_POST['address'];
+            $city = $_POST['city'];
+            $province = $_POST['province'];
+            $postalCode = $_POST['postalCode'];
+            $telephoneNumber = $_POST['telephoneNumber'];
+            $email = $_POST['email'];
+            $dateOfBirth = $_POST['dateOfBirth'];
+            $medicareExpiryDate = $_POST['medicareExpiryDate'];
+            $citizenship = $_POST['citizenship'];
+            if(empty($medicareCard) || empty($firstName) || empty($lastName) || empty($address) || empty($city) || empty($province) || empty($postalCode) || empty($telephoneNumber) || empty($email) || empty($dateOfBirth) || empty($medicareExpiryDate) || empty($citizenship))
+            {
+               echo "Please fill all the fields";
+            }
+            else
+            {
+                $sql = "INSERT INTO person (medicareCard, firstName, lastName, address, city, province, postalCode, telephoneNumber, email, dateOfBirth, medicareExpiryDate, citizenship)
+                VALUES ('$medicareCard', '$firstName', '$lastName', '$address', '$city', '$province', '$postalCode', '$telephoneNumber', '$email', '$dateOfBirth', '$medicareExpiryDate', '$citizenship')";
+            }
+        }
 ?>
