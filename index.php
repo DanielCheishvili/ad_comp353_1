ccb0dd3b45a49b80ec5850518898c4acec86acdf
DanielCheishvili <60618343+DanielCheishvili@users.noreply.github.com>
2023-08-02 14:31:03 -0400

    test

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
 
     <h1> Create A Person </h1>
-    <form action="createPerson.php" method="post">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
         <label for="medicareCard">Medicare Card</label>
         <input type="text" name="medicareCard" id="medicareCard" placeholder="Medicare Card">
         <br>
@@ -48,5 +48,50 @@
         <br>
         <input type="submit" name="submit" value="Submit">
     </form>
+
+    <?php 
+        include ('config.php');
+        $medicareCard = $firstName = $lastName = $address = $city = $province = 
+        $postalCode = $telephoneNumber = $email =  $dateOfBirth = $medicareExpiryDate = $citizenship = "";
+
+        $medicareCard_err = $firstName_err = $lastName_err = $address_err = $city_err = $province_err = $postalCode_err = 
+        $telephoneNumber_err = $email_err =  $dateOfBirth_err = $medicareExpiryDate_err = $citizenship_err = "";
+        echo "Hello";
+        echo $_POST['medicareCard'];
+        if(isset($_POST['submit']))
+        {
+            echo "not empty";
+            $medicareCard = $_POST['medicareCard'];
+            $firstName = $_POST['firstName'];
+            $lastName = $_POST['lastName'];
+            $address = $_POST['address'];
+            $city = $_POST['city'];
+            $province = $_POST['province'];
+            $postalCode = $_POST['postalCode'];
+            $telephoneNumber = $_POST['telephoneNumber'];
+            $email = $_POST['email'];
+            $dateOfBirth = $_POST['dateOfBirth'];
+            $medicareExpiryDate = $_POST['medicareExpiryDate'];
+            $citizenship = $_POST['citizenship'];
+            if(empty($medicareCard) || empty($firstName) || empty($lastName) || empty($address) || empty($city) || empty($province) || empty($postalCode) || empty($telephoneNumber) || empty($email) || empty($dateOfBirth) || empty($medicareExpiryDate))
+            {
+               echo "Please fill all the fields";
+            }
+            else
+            {
+                $sql = "INSERT INTO Person (medicareCard, firstName, lastName, address, city, province, postalCode, telephoneNumber, email, dateOfBirth, medicareExpiryDate, citizenship)
+                VALUES ('$medicareCard', '$firstName', '$lastName', '$address', '$city', '$province', '$postalCode', '$telephoneNumber', '$email', '$dateOfBirth', '$medicareExpiryDate', '1')";
+                if(mysqli_query($conn, $sql))
+                {
+                    echo "Record added successfully";
+                }
+                else
+                {
+                    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                }
+            }
+        }
+        mysqli_close($conn);
+?>
 </body>
 </html>
