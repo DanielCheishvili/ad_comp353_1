e2e5793acda6e322c9cc1df3cfe2a36ffaaa3294
DanielCheishvili <60618343+DanielCheishvili@users.noreply.github.com>
2023-08-03 22:58:37 -0400

    sad

--- Tables/person.php
+++ Tables/person.php
@@ -38,69 +38,71 @@
         </div>
     </nav>
     <button class="btn btn-primary" onclick="insertRow()">Insert Row</button>
-    <div class="container mt-5 mx-auto">
-        <h2>Person Table</h2>
-        <p>Table displaying all the records of the Person table.</p>
-       
-        <?php
-        include('../config.php'); 
-        
-        $sql = "SELECT * FROM Person";
-        $result = mysqli_query($conn, $sql);
-        
-        if (mysqli_num_rows($result) > 0) {
-            echo '<table class="table table-bordered">';
-            echo '<thead>';
-            echo '<tr>';
-            echo '<th>First Name</th>';
-            echo '<th>Last Name</th>';
-            echo '<th>Date Of Birth</th>';
-            echo '<th>Phone Number</th>';
-            echo '<th>Email</th>';
-            echo '<th>Address</th>';
-            echo '<th>City</th>';
-            echo '<th>Province</th>';
-            echo '<th>Postal Code</th>';
-            echo '<th>Medicare Card Number</th>';
-            echo '<th>Medicare Card Expiry Date</th>';
-            echo '<th>Citizenship</th>';
-            echo '<th>Actions</th>';
-            echo '</tr>';
-            echo '</thead>';
-            echo '<tbody>';
-        
-            while ($row = mysqli_fetch_assoc($result)) {
-                echo '<tr>';
-                echo '<td>' . $row['firstName'] . '</td>';
-                echo '<td>' . $row['lastName'] . '</td>';
-                echo '<td>' . $row['dateOfBirth'] . '</td>';
-                echo '<td>' . $row['telephoneNumber'] . '</td>';
-                echo '<td>' . $row['email'] . '</td>';
-                echo '<td>' . $row['address'] . '</td>';
-                echo '<td>' . $row['city'] . '</td>';
-                echo '<td>' . $row['province'] . '</td>';
-                echo '<td>' . $row['postalCode'] . '</td>';
-                echo '<td>' . $row['medicareCard'] . '</td>';
-                echo '<td>' . $row['medicareExpiryDate'] . '</td>';
-                echo '<td>' . ($row['citizenship'] ? 'Yes' : 'No') . '</td>';
-                echo '<td>';
-                echo '<button class="btn btn-danger" onclick="deletePerson(' . $row['medicareCard'] . ')">Delete</button>';
-                echo '<button class="btn btn-warning" onclick="editPerson(' . $row['medicareCard'] . ')">Edit</button>';
-                echo '</td>';
-                echo '</tr>';
-            }
-        
-            echo '</tbody>';
-            echo '</table>';
-        } else {
-            echo 'No records found.';
-        }
-        
-        mysqli_close($conn);
-        ?>
-
-       
-    </div>
+    <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <div class="table-responsive">
+                    <h2>Person Table</h2>
+                    <p>Table displaying all the records of the Person table.</p>
+                
+                    <?php
+                    include('../config.php'); 
+                    
+                    $sql = "SELECT * FROM Person";
+                    $result = mysqli_query($conn, $sql);
+                    
+                    if (mysqli_num_rows($result) > 0) {
+                        echo '<table class="table table-bordered">';
+                        echo '<thead>';
+                        echo '<tr>';
+                        echo '<th>First Name</th>';
+                        echo '<th>Last Name</th>';
+                        echo '<th>Date Of Birth</th>';
+                        echo '<th>Phone Number</th>';
+                        echo '<th>Email</th>';
+                        echo '<th>Address</th>';
+                        echo '<th>City</th>';
+                        echo '<th>Province</th>';
+                        echo '<th>Postal Code</th>';
+                        echo '<th>Medicare Card Number</th>';
+                        echo '<th>Medicare Card Expiry Date</th>';
+                        echo '<th>Citizenship</th>';
+                        echo '<th>Actions</th>';
+                        echo '</tr>';
+                        echo '</thead>';
+                        echo '<tbody>';
+                    
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo '<tr>';
+                            echo '<td>' . $row['firstName'] . '</td>';
+                            echo '<td>' . $row['lastName'] . '</td>';
+                            echo '<td>' . $row['dateOfBirth'] . '</td>';
+                            echo '<td>' . $row['telephoneNumber'] . '</td>';
+                            echo '<td>' . $row['email'] . '</td>';
+                            echo '<td>' . $row['address'] . '</td>';
+                            echo '<td>' . $row['city'] . '</td>';
+                            echo '<td>' . $row['province'] . '</td>';
+                            echo '<td>' . $row['postalCode'] . '</td>';
+                            echo '<td>' . $row['medicareCard'] . '</td>';
+                            echo '<td>' . $row['medicareExpiryDate'] . '</td>';
+                            echo '<td>' . ($row['citizenship'] ? 'Yes' : 'No') . '</td>';
+                            echo '<td>';
+                            echo '<button class="btn btn-danger" onclick="deletePerson(' . $row['medicareCard'] . ')">Delete</button>';
+                            echo '<button class="btn btn-warning" onclick="editPerson(' . $row['medicareCard'] . ')">Edit</button>';
+                            echo '</td>';
+                            echo '</tr>';
+                        }
+                    
+                        echo '</tbody>';
+                        echo '</table>';
+                    } else {
+                        echo 'No records found.';
+                    }
+                    
+                    mysqli_close($conn);
+                    ?>
+                </div>
+            </div>
+        </div>
 
     <script>
         function insertRow() {
