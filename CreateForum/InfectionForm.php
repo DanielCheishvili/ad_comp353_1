80b898daf0d2da9ddf919e7e444bc382a4c0ef79
DanielCheishvili <60618343+DanielCheishvili@users.noreply.github.com>
2023-08-07 20:28:57 -0400

    debugging

--- CreateForum/InfectionForm.php
+++ CreateForum/InfectionForm.php
@@ -96,6 +96,8 @@
                                 if($result)
                                 {
                                     echo "success";
+                                    var_dump($result);
+                                    
                                 }
                                 else
                                 {
