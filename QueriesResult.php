9368a7fe7d7b4871b9274e6ab9a05cf6a80ad343
DanielCheishvili <60618343+DanielCheishvili@users.noreply.github.com>
2023-08-11 14:45:58 -0400

    test

--- QueriesResult.php
+++ QueriesResult.php
@@ -1,45 +1,87 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="#">My Website</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/Tables/person.php">Person</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/Tables/employee.php">Employees</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/Tables/student.php">Students</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/Tables/facility.php">Facilities</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/Tables/vaccination.php">Vaccinations</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/Tables/infection.php">Infections</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/ShowQueries.php">Show Queries</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+<?php
+include('config.php');
+
+
+if (isset($_POST['queryNumber'])) {
+    $queryNumber = $_POST['queryNumber'];
     
-</body>
-</html>
+    $queryResult = ''; 
+    
+    switch ($queryNumber) {
+        case 1:
+            $queryResult = '<p>Query 1 result</p>';
+            $sql = "SELECT
+            Facility.facilityName,
+            Facility.address,
+            Facility.city,
+            Facility.province,
+            Facility.postalCode,
+            Facility.phoneNumber,
+            Facility.webAddress,
+            Facility.capacity,
+            Person.firstName,
+            Person.lastName,
+            COUNT(Employee.employeeID) AS NumberOfEmployees
+            FROM Facility
+            LEFT JOIN ManagementFacility ON ManagementFacility.facilityID = Facility.facilityID
+            LEFT JOIN EducationalFacility ON EducationalFacility.educationFacilityID = Facility.facilityID
+            LEFT JOIN Employee ON Employee.employeeID = ManagementFacility.presidentID OR Employee.employeeID = EducationalFacility.principalID
+            LEFT JOIN Person ON Person.medicareCard = Employee.medicareCard
+            GROUP BY
+            Facility.facilityName,
+            Facility.address,
+            Facility.city,
+            Facility.province,
+            Facility.postalCode,
+            Facility.phoneNumber,
+            Facility.webAddress,
+            Facility.capacity,
+            Person.firstName,
+            Person.lastName
+            ORDER BY 
+            Facility.province ASC, Facility.city ASC, Facility.facilityID ASC, NumberOfEmployees ASC
+            ";
+            $result = mysqli_query($conn, $sql);
+            //write me a table of all the results
+            if ($result && mysqli_num_rows($result) > 0) {
+                $queryResult .= '<table class="table table-bordered table-striped">';
+                $queryResult .= '<thead class="thead-dark"><tr><th scope="col">Facility Name</th><th scope="col">Address</th><th scope="col">City</th><th scope="col">Province</th><th scope="col">Postal Code</th><th scope="col">Phone Number</th><th scope="col">Web Address</th><th scope="col">Capacity</th><th scope="col">President/Principal</th><th scope="col">Number of Employees</th></tr></thead>';
+                $queryResult .= '<tbody>';
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $queryResult .= '<tr>';
+                    $queryResult .= '<td>' . $row['facilityName'] . '</td>';
+                    $queryResult .= '<td>' . $row['address'] . '</td>';
+                    $queryResult .= '<td>' . $row['city'] . '</td>';
+                    $queryResult .= '<td>' . $row['province'] . '</td>';
+                    $queryResult .= '<td>' . $row['postalCode'] . '</td>';
+                    $queryResult .= '<td>' . $row['phoneNumber'] . '</td>';
+                    $queryResult .= '<td>' . $row['webAddress'] . '</td>';
+                    $queryResult .= '<td>' . $row['capacity'] . '</td>';
+                    $queryResult .= '<td>' . $row['firstName'] . ' ' . $row['lastName'] . '</td>';
+                    $queryResult .= '<td>' . $row['NumberOfEmployees'] . '</td>';
+                    $queryResult .= '</tr>';
+                }
+                $queryResult .= '</tbody>';
+                $queryResult .= '</table>';
+            } else {
+                $queryResult .= '<p class="text-danger">No results found.</p>';
+            }
+           
+            break;
+        
+        case 2:
+            $queryResult = '<p>Query 2 result</p>';
+            break;
+        
+        
+        default:
+            $queryResult = '<p>No query result found.</p>';
+            break;
+    }
+    
+    echo $queryResult;
+}
+?>
+
+
+?>
